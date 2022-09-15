224972c77b0d31dc294c1b1e6c73276c45f9351c
WoodsonTD <taylodwoodson@gmail.com>
2022-09-15 12:18:22 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,8 @@
+// Hello, puts
+// print in C
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {	
-	puts("\"Programming is like building a multi#!/bin/bash
-# Simply a wrapper script to keep you from having to use betty-style
-# and betty-doc separately on every item.
-# Originally by Tim Britton (@wintermanc3r), multiargument added by
-# Larry Madeo (@hillmonkey)
-
-BIN_PATH="/usr/local/bin"
-BETTY_STYLE="betty-style"
-BETTY_DOC="betty-doc"
-
-if [ "$#" = "0" ]; then
-    echo "No arguments passed."
-    exit 1
-fi
-
-for argument in "$@" ; do
-    echo -e "\n========== $argument =========="
-    ${BIN_PATH}/${BETTY_STYLE} "$argument"
-    ${BIN_PATH}/${BETTY_DOC} "$argument"
-donelingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
 }
