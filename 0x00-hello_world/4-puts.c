252b0237530fef70fe9175312f85dfe8b12b8ee0
WoodsonTD <taylodwoodson@gmail.com>
2022-09-15 12:25:37 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-// Hello, puts
-// print in C
 #include <stdio.h>
+/**
+ * main - puts
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
