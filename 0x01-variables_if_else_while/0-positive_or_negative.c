390571ac91d02759c58b82bffafcc1cf4377cd30
WoodsonTD <taylodwoodson@gmail.com>
2022-09-19 12:48:35 -0700

    random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n= rand() - RAND_MAX / 2;
          if(n > 0)
 	 {
-	 printf("is positive.\n")
+	 printf("is positive.\n");
 	 }
          else if(n == 0)
 	 {
-	 printf("is zero.\n")
+	 printf("is zero.\n");
 	 }
          else if(n < 0)
 	 {
-	 pritf("is negative.\n")
+	 pritf("is negative.\n");
 	 }
         return (0);
 }
