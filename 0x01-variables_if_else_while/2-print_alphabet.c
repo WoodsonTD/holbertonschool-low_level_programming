f0cdb0bfb17024bc840a3b97b999ccbef7c31418
WoodsonTD <taylodwoodson@gmail.com>
2022-09-20 06:10:13 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - lowercase a-z
+ *Return: Always 0
+ */
+
+for(int x = 'A'; x <= 'Z'; x++)
+{
+    int lower_x = tolower(x);
+    putchar(lower_x);
+
+	return (0);
+}
