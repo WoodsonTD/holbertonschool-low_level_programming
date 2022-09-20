8a52e702e48081d0501fc3bf538fd0e06f482db7
WoodsonTD <taylodwoodson@gmail.com>
2022-09-20 07:20:09 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers with putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
