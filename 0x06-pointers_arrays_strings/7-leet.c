e30653801a571556ab92a08852ab343b323b840e
WoodsonTD <taylodwoodson@gmail.com>
2022-10-04 12:15:42 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * leet - write a function that encode a string into 1337
- * @s: pointer to string
- * Return: s
- */
-
+* leet - encodes a string to 1337
+* @s: input
+* Return: s
+*/
 char *leet(char *s)
 {
 	int i;
 	int v;
-	int find[] = { 'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l' };
-	int replace[] = { '4', '3', '0', '7', '1' };
-	
+	int find[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
+	int replace[] = {'4', '3', '0', '7', '1'};
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (v = 0; v <= 9; v++)
