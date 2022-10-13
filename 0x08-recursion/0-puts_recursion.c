b366ca231c8f28769efd0d7a3f72d420c3100f9d
WoodsonTD <taylodwoodson@gmail.com>
2022-10-13 08:50:34 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	char *s[] = "s";
+
+	puts(*s);
+
+	return 0;
+}
