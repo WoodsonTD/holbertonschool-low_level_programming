1682cd223a5b37213783b4f0ca1351dc6e95f9b0
WoodsonTD <taylodwoodson@gmail.com>
2022-10-26 09:21:33 -0700

    change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-int main(void);
+/*Prototypes*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
